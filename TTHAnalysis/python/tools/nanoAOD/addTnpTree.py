b6b8bcc201d741468f2c1a7fa70a71a7e4cfc9b9
Manos Vourliotis <emmanouil.vourliotis@gmail.com>
2019-10-10 16:06:27 +0200

    Modifications of Tag aNd Probe (TnP) module

--- TTHAnalysis/python/tools/nanoAOD/addTnpTree.py
+++ TTHAnalysis/python/tools/nanoAOD/addTnpTree.py
@@ -8,29 +8,22 @@ from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
 
 _rootLeafType2rootBranchType = { 'UChar_t':'b', 'Char_t':'B', 'UInt_t':'i', 'Int_t':'I', 'Float_t':'F', 'Double_t':'D', 'ULong64_t':'l', 'Long64_t':'L', 'Bool_t':'O' }
 
-def conept_TTH(lep):
-    if (abs(lep.pdgId)!=11 and abs(lep.pdgId)!=13): 
-        return lep.pt
-    if (abs(lep.pdgId)!=13 or lep.mediumId>0) and lep.mvaTTH > 0.90: 
-        return lep.pt
-    else: return 0.90 * lep.pt * (1+lep.jetRelIso)
-
 class addTnpTree(Module):
     def __init__(self, year, flavor):
         self.flavor = flavor
         if self.flavor == "Electron":
-            self.probeSel = lambda x : x.pt > 7 and abs(x.eta) < 2.5 and x.mvaFall17V2noIso_WPL
+            self.probeSel = lambda x : x.pt > 5 and abs(x.eta) < 2.5
             if year == 2016:
                 self.tagSel = lambda x : x.pt > 29 and x.cutBased > 3 and abs(x.eta) < 2.5
             if year == 2017:
-                self.tagSel = lambda x : x.pt > 37 and x.cutBased > 3 and abs(x.eta) < 2.5
+                self.tagSel = lambda x : x.pt > 37 and x.cutBased > 3 and abs(x.eta) < 2.5 # Should the "cutBased" be changed to "cutBased_Fall17_V1", since this is the ID used in this year?
             if year == 2018:
                 self.tagSel = lambda x : x.pt > 34 and x.cutBased > 3 and abs(x.eta) < 2.5
             self.kMaxMass = 140
             self.kMinMass = 60
 
         if self.flavor == "Muon":
-            self.probeSel = lambda x : x.pt > 5 and x.looseId and abs(x.eta) < 2.4
+            self.probeSel = lambda x : x.pt > 3 and x.looseId and abs(x.eta) < 2.4
             self.tagSel = lambda x : x.pt > 29 and x.tightId and abs(x.eta) < 2.4
             self.kMaxMass = 140
             self.kMinMass = 60
@@ -60,15 +53,7 @@ class addTnpTree(Module):
 
         ## Additional variables  added by hand 
         self.out.branch("Tag_isGenMatched", "I")
-        self.out.branch('Tag_jetBTagDeepB' ,'F')
-        self.out.branch('Tag_jetBTagDeepFlavB' ,'F')
-        self.out.branch('Tag_jetBTagCSVV2' ,'F')
-        self.out.branch('Tag_conept' ,'F')
         self.out.branch("Probe_isGenMatched",     "I")
-        self.out.branch('Probe_jetBTagDeepB' ,'F')
-        self.out.branch('Probe_jetBTagDeepFlavB' ,'F')
-        self.out.branch('Probe_jetBTagCSVV2' ,'F')
-        self.out.branch('Probe_conept' ,'F')
 
         self.out.branch("TnP_mass", "F")
         self.out.branch("TnP_ht",   "F")
@@ -116,17 +101,17 @@ class addTnpTree(Module):
         # Calculate event-per-event variables
         # Trigger requirement... IsoEle
         if self.flavor == "Electron":
-            if   self.year == 2017:
-                passTrigger = event.HLT_Ele35_WPTight_Gsf
-            elif self.year == 2016:
+            if   self.year == 2016:
                 passTrigger = event.HLT_Ele27_WPTight_Gsf
+            elif self.year == 2017:
+                passTrigger = event.HLT_Ele35_WPTight_Gsf
             elif self.year == 2018:
                 passTrigger = event.HLT_Ele32_WPTight_Gsf
         if self.flavor == "Muon":
-            if   self.year == 2017:
-                passTrigger = event.HLT_IsoMu27
-            elif self.year == 2016:
+            if   self.year == 2016:
                 passTrigger = event.HLT_IsoMu24 or event.HLT_IsoTkMu24
+            elif self.year == 2017:
+                passTrigger = event.HLT_IsoMu27
             elif self.year == 2018:
                 passTrigger = event.HLT_IsoMu24
             
@@ -143,7 +128,7 @@ class addTnpTree(Module):
         #### Selection of tag and probe electrons
         # Tag: pT, eta, tightId, iso
         # Probe: pT, eta
-        tags = []; probes = []; pair = []
+        pair = []
         index = 0
         for tag in lepton:
             if not self.tagSel(tag): continue
@@ -151,11 +136,11 @@ class addTnpTree(Module):
             probes = [] 
             for probe in lepton: 
                 if not self.probeSel(probe): continue
+                if probe.charge*tag.charge > 0: continue
                 mass = (probe.p4()+tag.p4()).M()
                 if mass > self.kMaxMass or mass < self.kMinMass: continue    
-                if probe.charge*tag.charge > 0: continue
-                probes.append(probe)
-            if len(probes) == 1: pair.append( (tag, probes[0]) )
+                probes.append(probe,mass)
+            if len(probes) == 1: pair.append( (tag, probes[0][0], probes[0][1]) )
 
         # Check that we have at least one pair... calculate the mass of the pair
         if len(pair) == 0: return False # events with 1 or 2 pairs!
@@ -165,28 +150,18 @@ class addTnpTree(Module):
 
         # Set variables for tag, probe and event
         for thisPair in pair:
-          tag, probe = thisPair
-          mass = (probe.p4()+tag.p4()).M()
+          tag, probe, mass = thisPair
 
           for branch in self.lepBranches:
               branchName = branch[0]
               self.out.fillBranch("Tag_%s"%branchName, getattr(tag,branchName))
               self.out.fillBranch("Probe_%s"%branchName, getattr(probe,branchName))
               
-              
           tagMatch = 1 if isdata else self.matchesPrompt(tag,genparts)
           self.out.fillBranch("Tag_isGenMatched", tagMatch)
-          self.out.fillBranch('Tag_jetBTagDeepB'    , 0 if tag.jetIdx < 0 else jet[tag.jetIdx].btagDeepB)
-          self.out.fillBranch('Tag_jetBTagDeepFlavB', 0 if tag.jetIdx < 0 else jet[tag.jetIdx].btagDeepFlavB)
-          self.out.fillBranch('Tag_jetBTagCSVV2'    , 0 if tag.jetIdx < 0 else jet[tag.jetIdx].btagCSVV2)     
-          self.out.fillBranch('Tag_conept',       conept_TTH(tag))
 
           probeMatch = 1 if isdata else  self.matchesPrompt(probe, genparts)
           self.out.fillBranch("Probe_isGenMatched", probeMatch)
-          self.out.fillBranch('Probe_jetBTagDeepB',     0 if probe.jetIdx < 0 else jet[probe.jetIdx].btagDeepB)     
-          self.out.fillBranch('Probe_jetBTagDeepFlavB', 0 if probe.jetIdx < 0 else jet[probe.jetIdx].btagDeepFlavB) 
-          self.out.fillBranch('Probe_jetBTagCSVV2',     0 if probe.jetIdx < 0 else jet[probe.jetIdx].btagCSVV2)     
-          self.out.fillBranch('Probe_conept',       conept_TTH(probe))
 
           # TnP variables
           self.out.fillBranch("TnP_mass",     mass);
